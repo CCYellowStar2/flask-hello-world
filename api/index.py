559499ef0edef805687c3f182f989816b9bb23b6
CCYellowStar2 <149048350+CCYellowStar2@users.noreply.github.com>
2023-12-14 21:01:31 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import json
 import requests
+import time
 
 app = Flask(__name__)
 
@@ -10,14 +11,21 @@ def home():
     p = request.args.get('p', default= 1, type=int)
     url = 'https://api.bilibili.com/x/web-interface/view/detail'
     params = {'bvid': bv}
-    response = requests.get(url, params=params)
-    
-    if response.status_code == 200:
-        json_data = response.json()
-        cid = json_data["data"]["View"]["pages"][p-1]["cid"]
-        print(cid)
-        return f"{cid}. Status code: {response.status_code}"
-    else:
+    max_retries = 3
+    retry_count = 0
+    while retry_count < max_retries:
+        response = requests.get(url, params=params)
+        
+        if response.status_code == 200:
+            json_data = response.json()
+            cid = json_data["data"]["View"]["pages"][p-1]["cid"]
+            print(cid)
+            return f"{cid}. Status code: {response.status_code}"
+        else:
+            print(f"Failed to fetch data. Status code: {response.status_code}. Retrying...")
+            retry_count += 1
+            time.sleep(1)  # 
+    if retry_count == max_retries:
         return f"Failed to fetch data. Status code: {response.status_code}"
     
 
