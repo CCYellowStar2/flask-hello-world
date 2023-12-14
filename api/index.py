fdb2a6fa9cb419a3fc7e6adcebca1c98c9e1c4f1
CCYellowStar2 <149048350+CCYellowStar2@users.noreply.github.com>
2023-12-14 20:19:16 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,23 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    bv = request.args.get('bv')
+    p = request.args.get('p', default= 1, type=int)
+    url = 'https://api.bilibili.com/x/web-interface/view/detail'
+    params = {'bvid': bv}
+    response = requests.get(url, params=params)
+    
+    if response.status_code == 200:
+        json_data = response.json()
+        cid = json_data["data"]["pages"][p-1]["cid"]
+        return cid
+    else:
+        return f"Failed to fetch data. Status code: {response.status_code}"
+    
 
 @app.route('/about')
 def about():
