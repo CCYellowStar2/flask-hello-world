34f622e4cf469fe0ea8afce64c5d472e54a145b0
CCYellowStar2 <149048350+CCYellowStar2@users.noreply.github.com>
2023-12-14 21:35:36 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 import json
 import requests
 import time
+import xml.etree.ElementTree as ET
 
 app = Flask(__name__)
 
@@ -13,18 +14,33 @@ def home():
     params = {'bvid': bv}
     max_retries = 5
     retry_count = 0
+    cid=""
+    
     while retry_count < max_retries:
-        response = requests.get(url, params=params)
+        if not cid:
+            response = requests.get(url, params=params)
         
-        if response.status_code == 200:
-            json_data = response.json()
-            cid = json_data["data"]["View"]["pages"][p-1]["cid"]
-            print(cid)
-            return f"{cid}. Status code: {response.status_code}"
+        if cid or response.status_code == 200:
+            if not cid:
+                json_data = response.json()
+                cid = json_data["data"]["View"]["pages"][p-1]["cid"]
+                print(cid)
+            url2= f"https://comment.bilibili.com/{cid}.xml"
+            response2 = requests.get(url2)
+            if response2.status_code == 200:
+                root = ET.fromstring(xml_data)
+                for d in root.findall('d'):
+                    p_value = d.attrib['p']
+                    text = d.text                
+            else:
+                print(f"Failed to fetch data. Status code: {response2.status_code}. Retrying...")
+                retry_count += 1
+                time.sleep(1)  
+            break
         else:
             print(f"Failed to fetch data. Status code: {response.status_code}. Retrying...")
             retry_count += 1
-            time.sleep(1)  # 
+            time.sleep(1)  
     if retry_count == max_retries:
         return f"Failed to fetch data. Status code: {response.status_code}"
     
