bcd70dd9e1758786ab51807be9b611ab12228b28
CCYellowStar2 <149048350+CCYellowStar2@users.noreply.github.com>
2023-12-17 19:28:05 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -78,7 +78,14 @@ def home():
             time.sleep(1)  
     if retry_count == max_retries:
         return f"Failed to fetch data. Status code: {response.status_code}"
-    
+
+@app.route('/live')
+def live():
+    id = request.args.get('id')
+    url = f'https://api.live.bilibili.com/room/v1/Room/get_info?room_id={id}'
+    response = requests.get(url, headers=headers)
+    json_data = response.json()
+    return json_data
 
 @app.route('/search')
 def search():
